0e09b26bc6589eb9337b89853bab929728be92e1
Ruamchok Mungphuengklang <i-tototids@hotmail.com>
2013-10-28 20:31:03 +0700

    test

---
+++ src/main/java/sut/se/toto/web/Tsetcomproser.java
@@ -0,0 +1,13 @@
+package sut.se.toto.web;
+
+import org.zkoss.zk.roo.annotations.ZKComposer;
+import org.zkoss.zk.ui.Component;
+
+@ZKComposer(zul = "tsst.zul")
+public class Tsetcomproser {
+
+    private static final long serialVersionUID = -7727182887440436881L;
+
+    public void afterCompose(Component comp) {
+    }
+}
